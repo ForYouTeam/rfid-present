d8a33513de79c87eb328b54d608241563c717a54
IPKIN9 <irwandirini@gmail.com>
2023-09-27 00:34:52 +0700

    Fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        $this->call([
-        PositionSeeder::class
+        PositionSeeder::class,
+        SatkerSeeder::class
        ]);
     }
 }
