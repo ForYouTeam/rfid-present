2d8b90ecc5ea55003b67a95239c596c71da9760e
IPKIN9 <irwandirini@gmail.com>
2023-09-27 07:46:43 +0700

    Done for create seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
        $this->call([
         PositionSeeder::class,
-        SatkerSeeder::class
+        SatkerSeeder::class,
+        RulesSeeder::class
        ]);
     }
 }
